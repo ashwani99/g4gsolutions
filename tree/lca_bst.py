3c73e40bcef953776316ade2b01d45f0f62fa5f4
Ashwani Gupta <ashwanigupta125@gmail.com>
2019-11-04 09:11:51 +0530

    fix: improve implementation of LCA for BST

--- tree/lca_bst.py
+++ tree/lca_bst.py
@@ -16,7 +16,7 @@ class BinarySearchTree(object):
             self.root = newnode
             return
 
-        ptr = root
+        ptr = self.root
         ptr_parent = None
         while ptr:
             if ptr.data == data:
@@ -39,7 +39,51 @@ class BinarySearchTree(object):
             BinarySearchTree.inorder(root.left)
             print(root.data)
             BinarySearchTree.inorder(root.right)
+    
+    def get_path_from_root(self, node_data):
+        stack = []
+        ptr = self.root
+        while ptr:
+            stack.append(ptr.data)
+            if ptr.data > node_data:
+                ptr = ptr.left
+            elif ptr.data < node_data:
+                ptr = ptr.right
+            else:
+                break
+        
+        return stack, len(stack)
 
-    @staticmethod
-    def LCS(node, other_node):
-        pass
+    def LCS(self, node, other_node):
+        path1, count1 = self.get_path_from_root(node)
+        path2, count2 = self.get_path_from_root(other_node)
+        
+        if count1 == 0 or count2 == 0:
+            print('Not able to find nodes')
+            return
+
+        while count1 != count2:
+            if count1 > count2:
+                path1.pop()
+                count1 -= 1
+            else:
+                path2.pop()
+                count2 -= 1
+
+        while path1[-1] != path2[-1]:
+            path1.pop()
+            path2.pop()
+
+        return path1[-1]
+    
+
+if __name__ == '__main__':
+    bst = BinarySearchTree()
+    bst.add_node(5)
+    bst.add_node(3)
+    bst.add_node(4)
+    bst.add_node(2)
+    bst.add_node(1)
+
+    BinarySearchTree.inorder(bst.root)
+    print(bst.LCS(1, 4))
